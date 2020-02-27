7029002c32216180bb775b066ca5b52eb12df806
Keisha Louise Vivien Berondo <berondokeisha@gmail.com>
2020-02-27 19:47:10 +0800

    Form is able to send post request.

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+cors = CORS(app, resources={r'/cpu-scheduling': {"origins": "*"}})
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/cpu-scheduling', methods=['post', 'get'])
+def schedule_processes():
+    return 'sksdvk'
+    # return ['hello', 'world']
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
--- client/src/components/Scheduling.vue
+++ client/src/components/Scheduling.vue
@@ -13,18 +13,33 @@
         <input id='arrival-time-input' ref='arrivalTimeInput' type='integer' />
         <label>Burst time</label>
         <input id='burst-time-input' ref='burstTimeInput' type='integer' />
-        <button id='add-process-btn' @click='addProcess'>Add Process</button>
+        <button id='add-process-btn' @click='addProcess' type='button'>Add Process</button>
       </div>
+      <ul id='process-list'>
+        <li class='process' v-for='(item, index) in processes' v-bind:key=index>
+          <div>
+            <span class='list-label'>Process Number</span>
+            <span class='process-no'>{{ item.process }}</span>
+          </div>
+          <div>
+            <span class='list-label'>Arrival time</span>
+            <span class='process-arrival'>{{ item.arrival }}</span>
+          </div>
+          <div>
+            <span class='list-label'>Burst time</span>
+            <span class='process-burst'>{{ item.burst }}</span>
+          </div>
+        </li>
+      </ul>
       <button id='algo-form-submit-btn'
         ref='algoFormSubmitBtn'
         @click='submitAlgoForm'
+        type='button'
         class='btn btn-primary'>
         Graph
       </button>
     </form>
-    <div id='process-list'>
-      {{ processes }}
-    </div>
+    {{ calculations }}
   </div>
 </template>
 
@@ -37,13 +52,14 @@ export default {
     return {
       processes: [],
       processCount: 1,
+      calculations: [],
     };
   },
   methods: {
     addProcess() {
       const [arrival, burst] = [
         this.$refs.arrivalTimeInput.value,
-        this.$refs.arrivalTimeInput.value,
+        this.$refs.burstTimeInput.value,
       ];
       if (burst && arrival) {
         this.processes.push({
@@ -55,10 +71,11 @@ export default {
       }
     },
     submitAlgoForm() {
-      const path = 'http://localhost:5000/ping';
-      axios.get(path)
+      const path = 'http://localhost:5000/cpu-scheduling';
+      axios.post(path, this.processes)
         .then((res) => {
-          this.msg = res.data;
+          this.calculations = res.data;
+          console.log('calculations', this.calculations);
         })
         .catch((error) => {
           // eslint-disable-next-line
